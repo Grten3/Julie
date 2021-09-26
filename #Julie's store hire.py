a73f6dd7107a964e3da5ca19fab7807bf9b8ab5a
Grten3 <78826655+Grten3@users.noreply.github.com>
2021-09-26 23:40:55 +1300

    V 1.6
    Bug fixes and adds one more parameter for component 2

--- #Julie's store hire.py
+++ #Julie's store hire.py
@@ -1,4 +1,4 @@
-#Julie's store hire program f
+#Julie's store hire program 
 #This program is used to store customer purchase data so that inventory of the store can be track
 
 
@@ -16,16 +16,12 @@ def quit(): #quit command
 def print_purchase_details ():
     global total_entries, name_count
 
-
     name_count = 0
     Label(main_window, font='bold',text="Row").grid(column=0,row=10, pady=(20,5))
     Label(main_window, font='bold',text="Full Name").grid(column=1,row=10, pady=(20,5))
     Label(main_window, font='bold',text="Receipt No.").grid(column=2,row=10, pady=(20,5))
     Label(main_window, font='bold',text="Item Hired").grid(column=3,row=10, pady=(20,5))
     Label(main_window, font='bold',text="Quantity").grid(column=4,row=10, padx=(35,0), pady=(20,5))
-
-
-
 
     while name_count < total_entries :
         Label(main_window, text=name_count).grid(column=0,row=name_count+11)
@@ -38,11 +34,10 @@ def print_purchase_details ():
 
 
 
+
 #add the next purchase details to the list if all parameters are meet
 def append_details ():
-    global purchase_details, full_name, receipt_no, item_hired, quantity, total_entries, full_name_first, full_name_blank, receipt_no_string, receipt_no_blank, item_hired_blank, quantity_blank, quantity_limit, quantity_letter
-
-
+    global purchase_details, full_name, receipt_no, item_hired, quantity, total_entries, full_name_first, full_name_blank, receipt_no_string, receipt_no_blank, receipt_no_special, item_hired_blank, quantity_blank, quantity_limit, quantity_letter
 
 #Check if the full name entry is blank 
     if len(re.findall(r'\w+', full_name.get())) == 0:
@@ -69,21 +64,31 @@ def append_details ():
     if len(re.findall(r'\w+', receipt_no.get())) == 0:
         receipt_no_blank.destroy()
         receipt_no_string.destroy()
+        receipt_no_special.destroy()
         receipt_no_blank = Label(main_window, text="*This can't be blank. Enter the Receipt Number", fg="red")
         receipt_no_blank.grid(row=2, column=3, columnspan=3)
     
-        
 #Check if there's a letter in the receipt number entry 
     if len(re.findall(r'\w+', receipt_no.get())) != 0:  
-        receipt_no_blank.destroy()
-        receipt_no_string.destroy()
-        if receipt_no.get().isdecimal() == False:
-            receipt_no_string = Label(main_window, text="*A letter is entered. Enter the Receipt Number only", fg="red")
+        if receipt_no.get().strip().isdecimal() == False:
+            receipt_no_blank.destroy()
+            receipt_no_string.destroy()
+            receipt_no_special.destroy()
+            receipt_no_string = Label(main_window, text="*A letter is entered. Enter the Receipt No. only", fg="red")
             receipt_no_string.grid(row=2, column=3, columnspan=3)
     
-    if (receipt_no.get().isdecimal()) == True:
+    if len(re.findall(r'\w+', receipt_no.get())) != 0:     
+        if receipt_no.get().strip().isalnum() == False:
+            receipt_no_blank.destroy()
+            receipt_no_string.destroy()
+            receipt_no_special.destroy()
+            receipt_no_string = Label(main_window, text="*A special character or a space is entered. Enter the Receipt No. only", fg="red")
+            receipt_no_string.grid(row=2, column=3, columnspan=3)
+
+    if receipt_no.get().strip().isdecimal() == True:
         receipt_no_blank.destroy()
         receipt_no_string.destroy()
+        receipt_no_special.destroy()
      
 
 #check if item hired entry is blank
@@ -105,51 +110,49 @@ def append_details ():
         quantity_blank = Label(main_window, text="*This can't be blank. Enter the Quantity", fg="red")
         quantity_blank.grid(row=4, column=3, columnspan=3)
 
+#check if quantity is blank 
     if len(re.findall(r'\w+', quantity.get())) != 0:  
         quantity_blank.destroy()
         quantity_letter.destroy()
         quantity_limit.destroy()
-        print("hi")
-
+        
+        #if quanity is not blank then try converting it to an int
         try:
              inter = int(quantity.get())
-             if 501 < int(quantity.get()) or int(quantity.get()) < 0:
+             if 500 < int(quantity.get()) or int(quantity.get()) < 0:
                 quantity_blank.destroy()
                 quantity_letter.destroy()
                 quantity_limit.destroy()
-                quantity_limit = Label(main_window, text="*Invalid value is entered. Enter a Quantity between 1 - 600", fg="red")
+                quantity_limit = Label(main_window, text="*Invalid value is entered. Enter a Quantity between 1 - 500", fg="red")
                 quantity_limit.grid(row=4, column=3, columnspan=3)
 
                 if 501 > int(quantity.get()) > 0:
                     quantity_blank.destroy()
                     quantity_letter.destroy()
                     quantity_limit.destroy()
-             
+
+        #if quantity cannot be converted to an int then display a custom error message     
         except ValueError:
-            quantity_letter = Label(main_window, text="*A letter is entered. Enter the Quantity Number only", fg="red")
+            quantity_letter = Label(main_window, text="*Invalid value is entered. Only enter the Quantity", fg="red") 
             quantity_letter.grid(row=4, column=3, columnspan=3)
-            print("hi2")
+            
     
-    
-    if quantity.get().isdecimal() == True:
-        inter = int(quantity.get())
-
-        
-
-       
+#Remember to fix issues with .isdecimal identifying spaces
 
 #append details if all requirements are met 
     if len(re.findall(r'\w+', full_name.get())) > 1:
-        if receipt_no.get().isdecimal() == True:
+        if receipt_no.get().strip().isdecimal() == True:
             if len(re.findall(r'\w+', item_hired.get())) > 0:
-                    if quantity.get().isdecimal() == True: #fix tommorow 
-                        if 501 > inter > 0:
+                    if quantity.get().strip().isdecimal() == True: 
+                         if 501 > int(quantity.get()) > 0:
                             purchase_details.append([full_name.get().title(),receipt_no.get(),item_hired .get(),quantity.get()]) 
                             full_name.delete(0,'end')
                             receipt_no.delete(0,'end')
                             item_hired.delete(0,'end')
                             quantity.delete(0,'end')
                             total_entries +=  1
+                            print("hi")
+                       
             
       
         
@@ -163,9 +166,9 @@ def delete_row ():
     delete_item.delete(0,'end')
     Label(main_window, text="          ").grid(column=0,row=name_count+10)
     Label(main_window, text="                      ").grid(column=1,row=name_count+10)
-    Label(main_window, text="          ").grid(column=2,row=name_count+10)
-    Label(main_window, text="          ").grid(column=3,row=name_count+10)
-    Label(main_window, text="          ").grid(column=4,row=name_count+10)
+    Label(main_window, text="               ").grid(column=2,row=name_count+10)
+    Label(main_window, text="               ").grid(column=3,row=name_count+10)
+    Label(main_window, text="               ").grid(column=4,row=name_count+10)
     print_purchase_details()
 
 
@@ -183,8 +186,6 @@ def GUI(): #Main GUI of the program
     Label(main_window, text="Full Name") .grid(row=1, column=1, sticky=E)
     full_name = Entry(main_window) #Text entry box for lengths
     full_name.grid(row=1, column=2, padx=10, pady=2, columnspan=2, sticky=W)
-    
-  
 
     Label(main_window, text="Receipt No.") .grid(row=2, column=1, sticky=E)
     receipt_no = Entry(main_window) #Text entry box for width
@@ -198,8 +199,6 @@ def GUI(): #Main GUI of the program
     quantity = Entry(main_window) #Text entry box for height
     quantity.grid(row=4, column=2, padx=10, pady=2, columnspan=2, sticky=W)
 
-
-
     Label(main_window, text="") .grid(row=7, column= 1, columnspan=4, pady=2)
 
     Label(main_window, text="Row No.") .grid(column=1,row=8, padx=0, pady= 5, sticky=E)
@@ -210,11 +209,12 @@ def GUI(): #Main GUI of the program
 
 
 def placeholder():
-    global full_name_first, full_name_blank, receipt_no_string, receipt_no_blank, item_hired_blank, quantity_blank, quantity_letter, quantity_limit
+    global full_name_first, full_name_blank, receipt_no_string, receipt_no_blank, receipt_no_special, item_hired_blank, quantity_blank, quantity_letter, quantity_limit
     full_name_blank = Label(main_window, text="")
     full_name_first = Label(main_window, text="")
     receipt_no_string = Label(main_window, text="")
     receipt_no_blank = Label(main_window, text="")
+    receipt_no_special = Label(main_window, text="")
     item_hired_blank = Label(main_window, text="")
     quantity_blank = Label(main_window, text="")
     quantity_letter = Label(main_window, text="")
